2b58af5033cd369d828ae04d6bd7e6fcd6da2b64
Ryan Smyth <rynsmyth@gmail.com>
2015-06-04 20:43:10 -0600

    Added Readme and ex2.rb
    [#95933786]

---
+++ chapter2/ex2.rb
@@ -0,0 +1,30 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+
+puts 'I could have code like this.' # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+# Study Drills
+
+# 1) Find out if you were right about what the # character does and
+# make sure you know what it's called (octothorpe or pound character).
+
+# I double checked that the # symbol will comment out a line.
+# I did not know that you should have a space after the # symbol.
+
+# 2) Take your ex2.rb file and review each line going backward. Start
+# at the last line, and check each word in reverse against what you
+# should have typed.
+
+# The only errors I found is to not use "". You should use ''.
+
+# 3) Did you find more mistakes? Fix them.
+
+# I did not find anymore mistakes.
+
+# 4)Read what you typed above out loud, including saying each
+# character by its name. Did you find more mistakes? Fix them.
+
+# I did not find any mistakes.
