f891fb2f0ce580569b416c160d4d1b262a90a355
Ryan Smyth <rynsmyth@gmail.com>
2015-06-05 17:43:33 -0600

    forgot comments for ex4.rb
    [#95933790]

--- chapter4/ex4.rb
+++ chapter4/ex4.rb
@@ -1,10 +1,18 @@
+# This assigns 100 to cars
 cars = 100
+# This assigns 4.0 to space_in_a_car
 space_in_a_car = 4.0
+# This assigns 30 to drivers
 drivers = 30
+# This assigns 90 to passengers
 passengers = 90
+# This subtracts drivers from cars
 cars_not_driven = cars - drivers
+# This points cars_driven to drivers
 cars_driven = drivers
+# This assigns the output of cars_driven multiplied by space_in_a_car to carpool_capacity
 carpool_capacity = cars_driven * space_in_a_car
+# This assigns the output of passengers divided by cars_driven to average_passengers_per_car
 average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available."
