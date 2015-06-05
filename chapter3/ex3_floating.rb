6246fb9196f7b36b5e21abc7fe9ba5162bd83e29
Ryan Smyth <rynsmyth@gmail.com>
2015-06-04 23:17:58 -0600

    Added ex3.rb, ex3_test.rb, ex3_floating.rb, Readme.md, irb_calculator.md for Chapter 3
    [#95933788]

---
+++ chapter3/ex3_floating.rb
@@ -0,0 +1,37 @@
+# This line prints a string.
+puts 'I will now count my chickens:'
+
+# This shows string interpolation with integers.
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# This shows string interpolation with integers.
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# This prints a string.
+puts 'Now I will count the egg:'
+
+# This shows how ruby uses the oder of operations.
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.0
+
+# This prints a string.
+puts 'Is it true that 3 + 2 < 5 - 7?'
+
+# This compares 5 to -2.
+puts 3 + 2 < 5 - 7.0
+
+# This shows string interpolation with integers.
+puts "What is 3 + 2? #{3.0 + 2}"
+# This shows string interpolation with integers.
+puts "What is 5 - 7? #{5.0 - 7}"
+
+# This prints a string.
+puts 'Oh, that\'s why it\'s false.'
+
+# This prints a string.
+puts 'How about some more.'
+
+# This shows string interpolation with integers.
+puts "Is it greater? #{5.0 > -2}"
+# This shows string interpolation with integers.
+puts "Is it greater or equal? #{5.0 >= -2}"
+# This shows string interpolation with integers.
+puts "Is it less or equal? #{5 <= -2.0}"
