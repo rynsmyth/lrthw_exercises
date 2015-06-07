8498eb92318b65159c3a89d3963732a1944f1607
Ryan Smyth <rynsmyth@gmail.com>
2015-06-07 00:18:48 -0600

    Added ex6.rb and README for Chapter 6
    [#95933794]

---
+++ chapter_6/ex6.rb
@@ -0,0 +1,36 @@
+# This assigns 10 to the variable
+types_of_people = 10
+# This assigns a string to the variable
+x = "There are #{types_of_people} types of people."
+# This assigns a string to the variable
+binary = 'binary'
+# This assigns a string to the variable
+do_not = "don't"
+# This assigns a string to the variable
+y = "Those who know #{binary} and those who #{do_not}."
+
+# This outputs variable x to the screen
+puts x
+# This outputs variable y to the screen
+puts y
+
+# This outputs a string with string interpolation
+puts "I said: #{x}."
+# This outputs a string with string interpolation
+puts "I also said: '#{y}'."
+
+# This assigns a string to the variable
+hilarious = false
+# This assigns a string to the variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# This outputs variable joke_evaluation to the screen
+puts joke_evaluation
+
+# This assigns a string to the variable
+w = 'This is the left side of...'
+# This assigns a string to the variable
+e = 'a string with a right side.'
+
+# This concatenates two strings and outputs it to the screen
+puts w + e
