4a7e4a72b51812e24f94ced11995d61c6b01dc82
Ryan Smyth <rynsmyth@gmail.com>
2015-06-07 00:40:19 -0600

    Added ex7.rb and README for Chapter 7
    [#95933796]

---
+++ chapter_7/README.md
@@ -0,0 +1,4 @@
+###Please explain the difference between puts and print in your own words in the Readme.md.  
+
+The difference between `print` and `puts`. Is that `puts` will output to screen with     
+a newline added to is and `print` will output without a newline.
---
+++ chapter_7/ex7.rb
@@ -0,0 +1,39 @@
+# Outputs a string to the screen
+puts 'Mary had a little lamb.'
+# Outputs a string to the screen
+puts "Its fleece was white as #{'snow'}."
+# Outputs a string to the screen
+puts 'And everywhere that Mary went.'
+# This outputted (10) . to the screen
+puts '.' * 10 # what'd that do?
+
+# Assigns a string to the variable
+end1 = 'C'
+# Assigns a string to the variable
+end2 = 'h'
+# Assigns a string to the variable
+end3 = 'e'
+# Assigns a string to the variable
+end4 = 'e'
+# Assigns a string to the variable
+end5 = 's'
+# Assigns a string to the variable
+end6 = 'e'
+# Assigns a string to the variable
+end7 = 'B'
+# Assigns a string to the variable
+end8 = 'u'
+# Assigns a string to the variable
+end9 = 'r'
+# Assigns a string to the variable
+end10 = 'g'
+# Assigns a string to the variable
+end11 = 'e'
+# Assigns a string to the variable
+end12 = 'r'
+
+# watch that print vs. puts on this line what's it do?
+# This outputs the string to the screen
+print end1 + end2 + end3 + end4 + end5 + end6
+# This outputs the string to the screen
+puts end7 + end8 + end9 + end10 + end11 + end12
