9994a23306a810b1365369681a75077cd019b252
Ryan Smyth <rynsmyth@gmail.com>
2015-06-08 16:45:14 -0600

    Changed single-quotes for multi-line string

--- chapter_9/ex9.rb
+++ chapter_9/ex9.rb
@@ -6,9 +6,11 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}"
 
-puts '''
-  There\'s something going here.
+# rubocop:disable Style/StringLiterals
+puts """
+  There's something going here.
 With the three double-quotes.
-We\'ll be able to type as much as we like.
+We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
-'''
+"""
+# rubocop:enable Style/StringLiterals
