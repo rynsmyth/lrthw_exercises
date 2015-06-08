c05277bee6468ef1434b0fc7c72f102028174b83
Ryan Smyth <rynsmyth@gmail.com>
2015-06-08 00:12:35 -0600

    Fixed misc errors
    [#95933788]

--- chapter3/Readme.md
+++ chapter3/Readme.md
@@ -3,5 +3,5 @@ You need to use a "floating point" number, which is a number with a decimal poin
 as in 10.5, or 0.89, or even 3.0.
 
 This is beacouse when you are using whole numbers there can be no remander.   
-If want to by 5 movies that are 10 dollar a piece but you only have 45 dollars.  
+If want to by 5 movies that are 10 dollars a piece but you only have 45 dollars.  
 You can not buy 4 1/2 movies.
--- chapter3/ex3_floating.rb
+++ chapter3/ex3_floating.rb
@@ -1,15 +1,15 @@
 # This line prints a string.
 puts 'I will now count my chickens:'
 
-# This shows string interpolation with integers.
+# This shows string interpolation with floating points.
 puts "Hens #{25.0 + 30.0 / 6.0}"
-# This shows string interpolation with integers.
+# This shows string interpolation with floating points.
 puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # This prints a string.
-puts 'Now I will count the egg:'
+puts 'Now I will count the eggs:'
 
-# This shows how ruby uses the oder of operations.
+# This shows how ruby uses the order of operations.
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.0
 
 # This prints a string.
@@ -18,20 +18,20 @@ puts 'Is it true that 3 + 2 < 5 - 7?'
 # This compares 5 to -2.
 puts 3 + 2 < 5 - 7.0
 
-# This shows string interpolation with integers.
+# This shows string interpolation with floating points.
 puts "What is 3 + 2? #{3.0 + 2}"
-# This shows string interpolation with integers.
+# This shows string interpolation with floating points.
 puts "What is 5 - 7? #{5.0 - 7}"
 
 # This prints a string.
-puts 'Oh, that\'s why it\'s false.'
+puts "Oh, that's why it's false."
 
 # This prints a string.
 puts 'How about some more.'
 
-# This shows string interpolation with integers.
+# This shows string interpolation with floating points.
 puts "Is it greater? #{5.0 > -2}"
-# This shows string interpolation with integers.
+# This shows string interpolation with floating points.
 puts "Is it greater or equal? #{5.0 >= -2}"
-# This shows string interpolation with integers.
+# This shows string interpolation with floating points.
 puts "Is it less or equal? #{5 <= -2.0}"
